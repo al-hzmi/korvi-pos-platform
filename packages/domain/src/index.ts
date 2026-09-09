b2102b0c349f4f033bc5725879b30684abc56d57
al-hzmi <alhzmymohanad@gmail.com>
2026-09-10 00:41:10 +0300

    feat(zatca): export CSID validation authority

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -22,6 +22,7 @@ export * from './ids/uuidv7.js';
 export * from './zatca/tlv.js';
 export * from './zatca/base64.js';
 export * from './zatca/phase2.js';
+export * from './zatca/csid.js';
 export * from './ports/persistence.js';
 export * from './ports/auth.js';
 export * from './ports/search.js';
