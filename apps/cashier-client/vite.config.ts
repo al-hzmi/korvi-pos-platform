24c116a3751172749daa4e9acd483a08434cebe2
al-hzmi <alhzmymohanad@gmail.com>
2026-09-15 19:39:58 +0300

    feat(installed): configure production cashier bundle

---
+++ apps/cashier-client/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  build: {
+    target: 'es2022',
+    sourcemap: false,
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  clearScreen: false,
+});
